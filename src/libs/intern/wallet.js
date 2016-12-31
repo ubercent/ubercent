6758c6aaa6b09b50fb222a290394c9a5f3375a52
RobinLinus <robin_woll@capira.de>
2016-12-31 06:34:21 +0100

    Fix Ether.js api calls

--- src/libs/intern/wallet.js
+++ src/libs/intern/wallet.js
@@ -3,10 +3,10 @@ var Wallet = (function() {
     var gasLimit = 53000;
 
     function spend(amount, receiver, callback) {
-        var keys = Account.getKeyPairFromStorage();
+        var privateKey = Account.getKeyPairFromStorage().private;
         var tx = Ether.generateTx(receiver, amount, Account.nonce(), gasPrice, gasLimit);
         console.log('push', tx);
-        var signedTx = Ether.signTransaction(keys.private,tx);
+        var signedTx = Ether.signTransaction(privateKey,tx);
         console.log('signed', signedTx);
         Backend.pushTransaction(signedTx,callback);
     }
